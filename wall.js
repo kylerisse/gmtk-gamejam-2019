2ab365aa98919ec29cc83d27707c326d40e77916
Andrew Len <alen@helloinspire.com>
2019-08-03 19:25:24 -0700

    Walls ARE Back!

---
+++ wall.js
@@ -0,0 +1,10 @@
+class Wall {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.poly = rect(x, y, width, height);
+    }
+}
